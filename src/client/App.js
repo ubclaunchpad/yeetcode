fdd145b57bcc5d596a759201c7d46c62a52eae92
Sophie Berger <sophie.berger@tailored-apps.com>
2019-10-26 15:23:35 -0700

    Nav bar

--- src/client/App.js
+++ src/client/App.js
@@ -1,23 +1,14 @@
-import React, { Component } from 'react';
-import './app.css';
-import ReactImage from './assets/react.png';
+import React, { Component } from "react";
+import "./app.css";
+import NavBar from "./components/NavBar";
 
-export default class App extends Component {
-  state = { username: null };
-
-  componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
-  }
-
+class App extends Component {
   render() {
-    const { username } = this.state;
     return (
       <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
+        <NavBar />
       </div>
     );
   }
 }
+export default App;
