efcde77f13c07d8ac77ce161e4729f95c5f0fd78
Bebo0 <beboelhosary@hotmail.com>
2019-11-16 14:21:09 -0800

    Add descr panel skeleton

--- src/client/App.js
+++ src/client/App.js
@@ -1,22 +1,21 @@
+/* eslint-disable */
 import React, { Component } from 'react';
 import './app.css';
-import ReactImage from './assets/react.png';
+import DescrPanel from "./components/descr-panel";
 
 export default class App extends Component {
-  state = { username: null };
+// export App = () => {
 
-  componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
-  }
+  // componentDidMount() {
+  //   fetch('/api/getUsername')
+  //     .then(res => res.json())
+  //     .then(user => this.setState({ username: user.username }));
+  // }
 
   render() {
-    const { username } = this.state;
     return (
       <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
+        <DescrPanel />
       </div>
     );
   }
---
+++ src/client/components/descr-panel.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../app.css';
+
+// const useStyles = makeStyles({
+//   root: {
+//     width: '100%',
+//     maxWidth: 500,
+//   },
+// });
+
+export default function DescrPanel() {
+
+  return (
+      
+    <div className="flex-container">
+      <div>
+        <h1>
+          Reverse string
+        </h1>
+      Write a function that reverses a string. 
+      </div>
+    </div> 
+  
+
+  );
+}
