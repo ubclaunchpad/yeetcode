d405b9b745e8f9481bc90bbf583649106df7a8cd
liang liu <stuffbyliang@gmail.com>
2019-11-16 15:19:13 -0800

    merged with the template board

--- src/client/views/MainScreen/MainScreen.jsx
+++ src/client/views/MainScreen/MainScreen.jsx
@@ -1,8 +1,17 @@
 /* eslint-disable */
 import React from 'react';
-import NavBar from '../../components/NavBar'
+import NavBar from '../../components/NavBar';
+import Resizable from '../../components/Resizable';
+import CodeEditor from '../../components/CodeEditor';
+
+import TitleBlock from '../../components/LabelBlock';
+
 import { RightPanel } from '../../components';
-import './MainScreen.css'
+
+import {Box} from "@material-ui/core";
+
+import './MainScreen.css';
+
 
 class MainScreen extends React.Component {
     render() {
@@ -13,14 +22,28 @@ class MainScreen extends React.Component {
             </div>
             <div className="content-area">
               <div className="left-panel">
-
-                        Hi
+                <Box className="container" display="flex">
+                  <Resizable className="draggable-left">
+                    <>
+                      <TitleBlock name="description" />
+                      <p>LEFT</p>
+                    </>
+                  </Resizable>
+                  <Box className="draggable-right" display="flex" flexGrow={1}>
+                    <>
+                      <TitleBlock name="PineappleSlayer69" />
+                      <p>RIGHT</p>
+                    </>
+                  </Box>
+                </Box>
               </div>
               <div className="right-panel">
                 <RightPanel />
               </div>
-            </div> 
+            </div>
+            <CodeEditor />
           </div>
+
       );
     }
 }
